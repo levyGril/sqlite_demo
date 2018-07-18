01ef28327d787d25306ce5796eb71d9383e3cb89
levy <levy.ji@cicdata.com>
2018-07-18 23:30:42 +0800

    remove di

--- src/app.js
+++ src/app.js
@@ -2,11 +2,20 @@
 const express = require("express");
 const app = express();
 
-const CORSModule = require("@altrdpdgm/altrd-cors")({options}); //<= set custom response headers in a configuration object on a `headers` property.
-CORSModule.setOriginList(["http://localhost:8080", "http://116.62.145.100", "http://0.0.0.0"]);
+//const CORSModule = require("@altrdpdgm/altrd-cors")({options}); //<= set custom response headers in a configuration object on a `headers` property.
+//CORSModule.setOriginList(["http://localhost:8080", "http://116.62.145.100", "http://0.0.0.0"]);
 
 //Configure the Express application to use the CORSModule on all incoming requests.
-app.use(CORSModule.handleCORS);
+//app.use(CORSModule.handleCORS);
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://0.0.0.0');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials','true');
+    next();
+};
+app.use(allowCrossDomain)
 
 const bodyParser = require("body-parser");
 
