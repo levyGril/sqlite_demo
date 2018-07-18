0666e688e8a539a1ed21f9e02fef6fa95cc79326
levy <levy.ji@cicdata.com>
2018-07-19 00:20:59 +0800

    cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 //app.use(CORSModule.handleCORS);
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://0.0.0.0');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials','true');
